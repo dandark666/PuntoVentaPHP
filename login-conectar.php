f92a42c567710fa05423f79b3fe0a80d5dfa5b80
dandark666 <82452568+dandark666@users.noreply.github.com>
2025-09-23 21:17:37 -0600

    Update login-conectar.php
    documentación de código

--- login-conectar.php
+++ login-conectar.php
@@ -2,15 +2,16 @@
     if (isset($_POST['txtusuario'])) {
         $user = $_POST['txtusuario'];
         $password = $_POST['txtcontraseña'];
-
+        //conexion con BD
         include('conexion/conexion.php');
-
+        //Consulta dentro de la BD encriptada 
         $sql = "SELECT * FROM Usuario WHERE Usuario = '$user' AND Clave = MD5($password)";
 
         $ejeSql = mysqli_query($conexion, $sql);
         $regUsuario = mysqli_fetch_assoc($ejeSql);
 
         if (mysqli_num_rows($ejeSql) == 1) {
+            //obtencion del rol del usuario u admin 
             session_start();
             $_SESSION['user'] = $regUsuario['Usuario'];
             $_SESSION['rol'] = $regUsuario['idrol'];
