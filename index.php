5e181516ba399216db4cc51e759207b4515ec21b
dandark666 <82452568+dandark666@users.noreply.github.com>
2025-09-26 15:45:05 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login Inicio</title>
     
     <link rel="stylesheet" href="estilos/login2.css">
 </head>
