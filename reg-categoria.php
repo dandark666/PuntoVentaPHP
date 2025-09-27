16e1af6cd870375ab42775c034857ed4fea9570c
dandark666 <82452568+dandark666@users.noreply.github.com>
2025-09-27 08:02:22 -0600

    Update reg-categoria.php
    insercion de datos

--- reg-categoria.php
+++ reg-categoria.php
@@ -1,1 +1,14 @@
+<?php
+    $clav = $_POST['txtCat'];
+    $descrip = $_POST['txtdescripcion'];
 
+   $cons = "INSERT categoria VALUES ('$clav', '$descrip');";
+
+    include("conexion/conexion.php");
+    if(mysqli_query($conexion,$cons)){
+        header("location:categoria.php");
+    }else{
+        echo"PROBLEMAS AL REGISTRAR EL BECARIO";
+    }
+    mysqli_close($conexion);
+    ?>
